95d41b4f750f34cfc4046ac7e271cb5cf40f1167
wanhong <wanhongfu@gmail.com>
2016-03-15 21:01:43 +0800

    init checkin

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # p2p-book-library-backend
-SpringBoot-based app for p2p-book-library-frontend project
+SpringBoot-based API service for p2p-book-library-frontend project, it is duplicated from example module of [SpringSide4](https://github.com/springside/springside4).
+
+###  How to run:
+
+1. git clone https://github.com/springside/springside4.git
+2. mvn install
+3. git clone https://github.com/wanhongfu/p2p-book-library-frontend.git
+4. mvn spring-boot:run
+
+### Changes
+
+1. Upgrade Spring Boot from 1.2.7.RELEASE to 1.3.2.RELEASE
+2. Add CORS annotation for client JS 
+
+	
---
+++ src/main/java/org/springside/examples/bootapi/config/WebConfiguration.java
@@ -0,0 +1,35 @@
+/*********************************************************************************
+ * Copyright © 2015 Hierstar
+ *
+ * All Rights Reserved.
+ *********************************************************************************/
+
+package org.springside.examples.bootapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * 
+ * @author wanhong
+ * @date Mar 11, 2016
+ */
+@Configuration
+public class WebConfiguration {
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+		
+        return new WebMvcConfigurerAdapter() {
+        
+        	@Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**");
+            }
+        	
+        };
+    }	
+}
